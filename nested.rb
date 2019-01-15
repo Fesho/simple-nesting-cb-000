52d28d03fdb03fc5a3cc690460641b62fdcd86d8
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-15 18:59:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,7 @@ end
 hopper
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+
 
 	programmer_hash =
  		{
@@ -112,7 +112,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+  
      alans_new_info = "GUI"
      
   programmer_hash[:alan_kay][:known_for] = "GUI"
